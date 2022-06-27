e1e4519e6c1ba7069030860b31ecede3308010c2
Erwin Liong <eliong@rockwellautomation.com>
2022-06-27 17:03:34 +0800

    Update MongoDB link

--- envs.ts
+++ envs.ts
@@ -5,5 +5,5 @@ config();
 export default {
     port: process.env.PORT || 3000,
     graphqlPath: process.env.GRAPHQL_PATH || '/graphql',
-    dbUrl: process.env.MONGODB_URI,
+    dbUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/graphql-starter',
 }
