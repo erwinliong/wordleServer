baae980fe2b4aebdba7cfc5bc82b20adee8806c1
Erwin Liong <erwinliong@gmail.com>
2022-06-27 02:10:47 +0000

    Added hello world

--- build/index.js
+++ build/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 // Constants
 const app = (0, express_1.default)();
 /***********************************************************************************
@@ -12,7 +13,14 @@ const app = (0, express_1.default)();
 // Common middlewares
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+// Set views dir
+const viewsDir = path_1.default.join(__dirname, 'views');
+app.set('views', viewsDir);
 const serverStartMsg = 'Express server started on port: ', port = (process.env.PORT || 3000);
+// Serve index.html file
+app.get('*', (_, res) => {
+    res.send('<h1>Hello World</h1>');
+});
 // Start server
 app.listen(port, () => {
     console.log(serverStartMsg + port);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 // Constants
 const app = express();
@@ -11,8 +12,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set views dir
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+
 const serverStartMsg = 'Express server started on port: ',
     port = (process.env.PORT || 3000);
+
+// Serve index.html file
+app.get('*', (_: Request, res: Response) => {
+    res.send('<h1>Hello World</h1>');
+  });
 
 // Start server
 app.listen(port, () => {
