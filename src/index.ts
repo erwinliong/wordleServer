dd4aac90f491f0bdecc1727f13649ed50e267c10
Erwin Liong <eliong@rockwellautomation.com>
2022-06-27 10:20:25 +0800

    Push changes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,13 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
+import server from './server';
+
 
 // Constants
-const app = express();
-
-/***********************************************************************************
- *                                  Middlewares
- **********************************************************************************/
-
-// Common middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Set views dir
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-
 const serverStartMsg = 'Express server started on port: ',
-    port = (process.env.PORT || 3000);
-
-// Serve index.html file
-app.get('*', (_: Request, res: Response) => {
-    res.send('<h1>Hello World</h1>');
-  });
+        port = (process.env.PORT || 3000);
 
 // Start server
-app.listen(port, () => {
-    console.log(serverStartMsg + port);
+server.listen(port, () => {
+    logger.info(serverStartMsg + port);
 });
