30fe8335554011fa54e6ca5f1c43d72e68bce7c1
Erwin Liong <erwinliong@gmail.com>
2022-06-27 09:48:24 +0000

    Add wordOfTheDay

--- schema/schema.ts
+++ schema/schema.ts
@@ -2,29 +2,30 @@ import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
     type Query {
-        books(limit: Int): [Book]
-        book(id: ID!): Book
+        // books(limit: Int): [Book]
+        // book(id: ID!): Book
         allWords: [String]
+        wordOfTheDay(index: Int!): String
     }
-    type Mutation {
-        addBook(title: String!, author: String!, description: String!): BookResponse
-        updateBook(id: ID!, title: String, author: String, description: String): BookResponse
-        deleteBook(id: ID!): BookResponse
-    }
-    type Book {
-        _id: ID!
-        title: String!
-        author: String!
-        description: String!
-    }
-    type Books {
-        books: [Book]
-    }
-    type BookResponse {
-        data: Book
-        error: String
-        ok: Boolean
-    }
+    // type Mutation {
+    //     addBook(title: String!, author: String!, description: String!): BookResponse
+    //     updateBook(id: ID!, title: String, author: String, description: String): BookResponse
+    //     deleteBook(id: ID!): BookResponse
+    // }
+    // type Book {
+    //     _id: ID!
+    //     title: String!
+    //     author: String!
+    //     description: String!
+    // }
+    // type Books {
+    //     books: [Book]
+    // }
+    // type BookResponse {
+    //     data: Book
+    //     error: String
+    //     ok: Boolean
+    // }
 `);
 
 export default schema;
