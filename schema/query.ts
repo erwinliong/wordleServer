3f72cbe6df031a2ab765fa82f26ca8a525ec15f8
Erwin Liong <erwinliong@gmail.com>
2022-06-27 09:51:26 +0000

    return dummy string

--- schema/query.ts
+++ schema/query.ts
@@ -28,20 +28,8 @@ const query = {
         return result as string[];
     },
     wordOfTheDay: async (args: any) => {
-        const url = 'https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/ca9018b32e963292473841fb55fd5a62176769b5/valid-wordle-words.txt';
-        let promise = new Promise((resolve, reject) => {
-            let data: string | string[] = '';
-            https.get(url, res => {
-                res.on('data', chunk => { data += chunk })
-                res.on('end', () => {
-                    data = (data as string).split('\n');
-                    resolve(data);
-                })
-            })
-        });
-
-        return (await promise as string[])[args.index];
-    }
+        return 'adieu';
+    },
 };
 
 export default query
